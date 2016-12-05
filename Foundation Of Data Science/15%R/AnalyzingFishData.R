40f24f8104e87bd1607d10ad6217d8a421e64e48
shargaas <gtionogu@ulb.ac.be>
2016-12-05 18:35:04 +0000

    [R+Pentest][Mod] Added Ssh and sudo + advanced in R report

--- Foundation Of Data Science/15%R/AnalyzingFishData.R
+++ Foundation Of Data Science/15%R/AnalyzingFishData.R
@@ -48,7 +48,7 @@ stripchart(mydata$V1,add = TRUE, method="stack")
 
 #plots of 2 variables combined (x,y)
 plot(mydata$V1,mydata$V2,main="Distribution of catches", xlab = "Daytime (h.%h)", ylab = "Size of the catch(kg)")
-
+boxplot(mydata$V1,mydata$V2)
 #corelation
 cor(mydata$V1,mydata$V2)
 
@@ -92,9 +92,29 @@ getmode(mydata$V2)
 exp(mean(log(mydata$V1)))
 exp(mean(log(mydata$V2)))
 
+#variations
+var(mydata$V1)
+var(mydata$V2)
+
+#standard deviation
+sd(mydata$V1)
+sd(mydata$V2)
+
+#IQR
+IQR(mydata$V1)
+IQR(mydata$V2)
+
 #density
-hist(mydata$V1,prob=TRUE,breaks=c(0,seq(2.8,26,2.8)),ylim = c(0,0.06),xlim = c(0,24))
+hist(mydata$V1,main="Kernel density estimation for the times of catch",xlab="Daytime (hours)",prob=TRUE,breaks=c(0,seq(2.8,26,2.8)),ylim = c(0,0.06),xlim = c(0,24),xaxt="n")
+axis(1,at=c(seq(0,24,4)))
 lines(d)
+legend(15.2,0.06,c("N = 200","bandwidth = 1.764"))
+
+hist(mydata$V2,main="Kernel density estimation for the weight of catches",xlab="Daytime (hours)",prob=TRUE,breaks=18,ylim = c(0,0.5),xlim = c(0,5),xaxt="n")
+axis(1,at=c(seq(0,4.5,0.5)))
+lines(e)
+legend(0.5,0.5,c("N = 200","bandwidth = 0.3363"))
+
 
 skewness(mydata$V1)
 skewness(mydata$V2)
@@ -104,10 +124,16 @@ kurtosis(mydata$V2)
 
 #PROJECT
 #raw distributions
-hist(mydata$V1,main = "Distribution of the times of catch",xlab = "Daytime (hours)",axes = FALSE, ylab = "Count",breaks=c(seq(0,25.2,2.8)))
+hist(mydata$V1,main = "Distribution of the times of catch",xlab = "Daytime (hours)",axes = FALSE, ylab = "Count",breaks=c(seq(0,25.2,2.8)),prob =TRUE)
 axis(1,at=c(seq(0.0,24.0,4)))
 axis(2,at=c(seq(0.0,35.0,5)))
-boxplot(mydata$V1,horizontal = TRUE,add = TRUE, axes = FALSE, at=2.5,las=2)
-axis(1,at=c(seq(0,24,4)))
-hist(mydata$V2,main = "Distribution of the sizes of catch",xlab = "Weight of catches (kg)",ylab = "Count",breaks=c(0,seq(0.61,4.88,0.61)))
+boxplot(mydata$V1,horizontal = TRUE,add = TRUE, axes = FALSE, at=2.5)
+lines(d)
+hist(mydata$V2,main = "Distribution of the sizes of catch",xlab = "Weight of catches (kg)",axes = FALSE, ylab = "Count",breaks=c(seq(0,4.88,0.61)))
+axis(1,at=c(seq(0,4.5,0.25)))
+axis(2,at=c(seq(0.0,45,2.5)))
 boxplot(mydata$V2,horizontal = TRUE,add = TRUE, axes = FALSE, at=2.5)
+
+#boxplots of (x,y)
+n = cut(mydata$V1,24)
+plot(n,mydata$V2)
